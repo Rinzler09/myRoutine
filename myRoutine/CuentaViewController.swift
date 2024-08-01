79dc0dbd29a7068d5e0a50655642543e57db5e06
MiltonPaz <miltonnoelpaz.18@gmail.com>
2024-07-31 17:23:36 -0700

    Adicion pantalla crear usuario

--- myRoutine/CuentaViewController.swift
+++ myRoutine/CuentaViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class CuentaViewController: UIViewController {
-
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var btnCreate: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func actionCreate(_ sender: Any) {
     }
-    */
-
 }
